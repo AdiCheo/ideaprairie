a55464f3767b3715e1ef465fac360d84498fc1e0
Sebastian Tobiasz <sebastian_tobiasz_54@hotmail.com>
2016-08-26 17:43:41 +0000

    all ideas are shown, still needs filtering

--- client/app/profile/profile.controller.js
+++ client/app/profile/profile.controller.js
@@ -1,16 +1,16 @@
 'use strict';
-
+var getCurrentUser = '';
 (function(){
 
 class ProfileComponent {
   constructor($http, $scope, socket, $stateParams, Auth) {
      this.$http = $http;
     this.socket = socket;
-    this.getCurrentUser = Auth.getCurrentUser;
+    getCurrentUser = Auth.getCurrentUser;
   }
 
 
-  // $onInit(){
+  $onInit(){
   // Promise.all([
   //     this.$http.get('/api/user/'.getCurrentUser()._id)
   //       .then(response => {
@@ -18,7 +18,28 @@ class ProfileComponent {
   //         this.socket.syncUpdates('user', this.userDetails);
   //       })
   //   ]);
-  // }
+  this.$http.get('/api/ideas/')
+          .then(response => {
+            this.awesomeIdeas = response.data.filter(this.isRelatedToIdea); // Filter only relevant comments
+            this.socket.syncUpdates('idea', this.awesomeIdeas);
+          });
+  }
+  addFeedback(opinion, ideaId) {
+      if (opinion && ideaId) {
+        this.$http.post('/api/feedbacks', {
+          opinion: opinion,
+          idea: ideaId
+        });
+      }
+    }
+    deleteIdea(idea) {
+    this.$http.delete('/api/ideas/' + idea._id);
+  }
+  
+  isRelatedToIdea(commentObj) {
+    return commentObj.idea.user._id === getCurrentUser()._id;
+    // return true;
+  }
 }
 
 angular.module('ideaApp')
